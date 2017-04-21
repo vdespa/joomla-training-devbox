945332b3461db9180b1e20ff307efbca8f68aaa4
Valentin Despa <info@vdespa.de>
2017-04-21 22:28:04 +0200

    Temporary revert to 3.6.5
    Because of error during the installation

--- puphpet/files/exec-once/10-get-joomla-source.sh
+++ puphpet/files/exec-once/10-get-joomla-source.sh
@@ -13,7 +13,7 @@ echo "[DONE] Cloning the Jommla! Git repository."
 echo "[START] Copying files ... "
 git clone --reference /home/vagrant/gitcaches/joomla.reference https://github.com/joomla/joomla-cms.git joomla-development.local
 cd joomla-development.local
-git checkout staging
+git checkout 3.6.5
 git pull
 cp -r /var/www/joomla-development.local/. /var/www/joomla-testing.local
 echo "[DONE] Copying files."
