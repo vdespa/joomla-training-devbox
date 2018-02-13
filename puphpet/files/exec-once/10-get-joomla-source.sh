543eca8568abf98099b924a2473e9affe293f910
Valentin Despa <info@vdespa.de>
2018-02-13 05:30:17 +0100

    Remove the usage of local git cache
    This seems to be causing errors like error: object directory /home/vagrant/gitcaches/joomla.reference/objects does not exist;

--- puphpet/files/exec-once/10-get-joomla-source.sh
+++ puphpet/files/exec-once/10-get-joomla-source.sh
@@ -6,12 +6,8 @@ mkdir -p joomla-development.local
 mkdir -p joomla-testing.local
 echo "[DONE] Finished creating folders."
 
-echo "[START] Cloning the Jommla! Git repository (cache)..."
-git clone --mirror https://github.com/joomla/joomla-cms.git /home/vagrant/gitcaches/joomla.reference
-echo "[DONE] Cloning the Jommla! Git repository."
-
 echo "[START] Copying files ... "
-git clone --reference /home/vagrant/gitcaches/joomla.reference https://github.com/joomla/joomla-cms.git joomla-development.local
+git clone https://github.com/joomla/joomla-cms.git joomla-development.local
 cd joomla-development.local
 git checkout 3.6.5
 git pull
